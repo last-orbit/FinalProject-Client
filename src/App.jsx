60a7f07923efd66c7e974eb271ed3906f06c5002
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2024-12-11 16:43:39 +0100

    Day 2 - added our routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,53 @@ import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import HomePage from './pages/HomePage/HomePage';
 import AboutUs from './pages/AboutUs/AboutUs';
-
+import ImagePage from './pages/ImagePage/ImagePage';
+import MyCollection from './pages/CollectionPage/MyCollection';
 import { ProtectedRoute } from './components/ProtectedRoute';
+import NotFound from './pages/NotFound/NotFound';
+import SignupPage from './pages/SignUpPage/SignUp';
+
+import ShufflePage from './pages/ShuffePage/ShufflePage';
 
 function App() {
   return (
     <main>
       <Routes>
-        {/* <Route path='/' element={<SignupPage />} /> */}
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/' element={<ProtectedRoute>
-          <HomePage />
+        <Route path='/i-wanna-be-part-of-this' element={<SignupPage />} />
+        <Route path='/you-shall-not-pass' element={<LoginPage />} />
+        <Route
+          path='/'
+          element={
+              <HomePage />
+          }
+        />
+        <Route
+          path='/for-frodo'
+          element={
+            <ProtectedRoute>
+              <ImagePage />
+            </ProtectedRoute>
+          }
+        />
 
-        </ProtectedRoute>
-        }/>
-        <Route path='/about-us' element={<AboutUs />} />
-
+        <Route path='/fellowship' element={<AboutUs />} />
+        <Route
+          path='/my-precious'
+          element={
+            <ProtectedRoute>
+              <MyCollection />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/the-eagles-are-coming'
+          element={
+            <ProtectedRoute>
+              <ShufflePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </main>
   );
