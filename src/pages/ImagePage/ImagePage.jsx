cf7faa65c0172a29ff97c1e1ae89465bd417d8c8
Fabien Dubin <fabien.dubin@gmail.com>
2024-12-14 00:35:03 +0100

    testing stuff

--- src/pages/ImagePage/ImagePage.jsx
+++ src/pages/ImagePage/ImagePage.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/AuthContext';
-import { Button } from '@/components/ui/button';
-import { Textarea } from '@/components/ui/textarea';
-import axios from 'axios';
-import { API_URL } from '../../../config';
-import { data, useParams } from 'react-router-dom';
-import { Skeleton } from '@/components/ui/skeleton';
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import axios from "axios";
+import { API_URL } from "../../../config";
+import { data, useParams } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   Card,
   CardContent,
@@ -14,7 +14,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
+} from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
@@ -23,9 +23,9 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-} from '@/components/ui/dialog';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+} from "@/components/ui/dialog";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Headphones,
   Pencil,
@@ -34,7 +34,7 @@ import {
   CircleMinus,
   CircleX,
   SendHorizontal,
-} from 'lucide-react';
+} from "lucide-react";
 
 const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
   const { user } = useContext(AuthContext);
@@ -45,9 +45,9 @@ const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
   // grabs the comments
   const [comments, setComments] = useState();
   // Edits the comment
-  const [editComment, setEditComment] = useState('');
+  const [editComment, setEditComment] = useState("");
   // For New Comments
-  const [newComment, setNewComment] = useState('');
+  const [newComment, setNewComment] = useState("");
   // Checks if Image is in Collection
   const [isInCollection, setIsInCollection] = useState(false);
 
@@ -97,7 +97,7 @@ const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
         comment: newComment,
       });
       console.log(response);
-      setNewComment('');
+      setNewComment("");
     } catch (error) {
       console.log(error);
     }
@@ -125,19 +125,20 @@ const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
   useEffect(() => {
     getImage();
     // checkCollection();
-    const CheckIfInCollection = async () => {
+    const checkIfInCollection = async () => {
       // const body = { data: { userId: user._id, imageId: imageId } };
       try {
         const { data } = await axios.get(
           `${API_URL}/collection/isincollection`,
           { params: { userId: user._id, imageId: imageId } }
         );
-        // console.log(data);
+        console.log(data);
         setIsInCollection(data.isInCollection);
       } catch (error) {
         console.log(error);
       }
     };
+    checkIfInCollection();
     // if (user) {
     //   // console.log(CheckIfInCollection())
     //   // setIsInCollection(CheckIfInCollection());
@@ -147,12 +148,12 @@ const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
   /******************* If there are no images, show a loading screen **************/
   if (!currentImage) {
     return (
-      <div className='flex flex-col space-y-3 justify-self-center align-self-center mt-10'>
-        <Skeleton className='h-[250px] w-[300px] rounded-xl' />
-        <div className='space-y-4 justify-self-end'>
-          <Skeleton className='h-4 w-[250px] mt-7 justify-self-end' />
-          <Skeleton className='h-4 w-[200px] justify-self-end' />
-          <Skeleton className='h-4 w-[200px] justify-self-end' />
+      <div className="flex flex-col space-y-3 justify-self-center align-self-center mt-10">
+        <Skeleton className="h-[250px] w-[300px] rounded-xl" />
+        <div className="space-y-4 justify-self-end">
+          <Skeleton className="h-4 w-[250px] mt-7 justify-self-end" />
+          <Skeleton className="h-4 w-[200px] justify-self-end" />
+          <Skeleton className="h-4 w-[200px] justify-self-end" />
         </div>
       </div>
     );
@@ -161,13 +162,15 @@ const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
   return (
     <>
       {/* Image */}
-      <div className='mt-10  gap-2 m-0 w-7/12 justify-self-center  h-auto'>
-        <div className='mb-5'>
-          <img
-            className='h-1/3 w-96 object-contain justify-self-center drop-shadow bg-white rounded-2xl shadow-lg relative'
-            src={currentImage.photo_image_url}
-            alt='The Image goes here'
-          />
+      <div className="mt-10  gap-2 m-0 w-7/12 justify-self-center  h-auto">
+        <div className="mb-5">
+          <Card className="relative w-[80vw] max-w-[300px] h-[300px] bg-white rounded-2xl shadow-lg overflow-hidden">
+            <img
+              className="w-full h-full object-cover"
+              src={currentImage.photo_image_url}
+              alt="The Image goes here"
+            />
+          </Card>
         </div>
         {/* <Card className='relative w-[80vw] max-w-[300px] h-[300px] bg-white rounded-2xl shadow-lg overflow-hidden align-self-center'>
           <img
@@ -177,40 +180,44 @@ const ImagePage = ({ deleteImageToCollection, addImageToCollection }) => {
           />
         </Card> */}
         {/* Image details */}
-        <div className='pb-20'>
-          <div className='flex gap-2 justify-between'>
-            <p className='text-2xl font-semibold capitalize'>
-              {currentImage.ai_description}{' '}
-            </p>{' '}
+        <div className="pb-20">
+          <div className="flex gap-2 justify-between">
+            <p className="text-2xl font-semibold capitalize">
+              {currentImage.ai_description}{" "}
+            </p>{" "}
             <p>
-              {' '}
-              Photo By : {currentImage.photographer_first_name}{' '}
+              {" "}
+              Photo By : {currentImage.photographer_first_name}{" "}
               {currentImage.photographer_last_name}
-            </p>{' '}
+            </p>{" "}
           </div>
           {/* Bottom Section */}
-          <div className='flex gap-2 justify-self-end h-48 '>
+          <div className="flex gap-2 justify-self-end h-48 ">
             <Button
-              className='w-fit my-5 '
-              variant={isInCollection ? 'secondary' : ''}
+              className="w-fit my-5 "
+              variant={isInCollection ? "secondary" : ""}
               onClick={() => {
                 isInCollection
-                  ? (deleteImageToCollection(imageId) , setIsInCollection(false))
-                  : (addImageToCollection(imageId) , setIsInCollection(true));
+                  ? (deleteImageToCollection(imageId), setIsInCollection(false))
+                  : (addImageToCollection(imageId), setIsInCollection(true));
               }}
             >
-              {' '}
-              {isInCollection ? <CircleMinus /> : <CirclePlus />}{' '}
-              {isInCollection ? 'Remove from Collection' : 'Add to Collection'}
-            </Button>{' '}
+              {" "}
+              {isInCollection ? <CircleMinus /> : <CirclePlus />}{" "}
+              {isInCollection ? "Remove from Collection" : "Add to Collection"}
+            </Button>{" "}
           </div>
           {/* Add/Remove Image from Collection */}
 
           {/* Add/Remove Comment This is what Robert wrote */}
-          <div className='grid w-full gap-2 mb-15'>
+          <div className="grid w-full gap-2 mb-15">
             <h4>Add your comment below</h4>
-            <Textarea placeholder='Type your comment here.' value={newComment} onChange={(e) => setNewComment(e.target.value)} />
-            <Button className='w-fit justify-self-end'>
+            <Textarea
+              placeholder="Type your comment here."
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+            />
+            <Button className="w-fit justify-self-end">
               Send comment
               <SendHorizontal />
             </Button>
