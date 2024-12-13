2e106211e8946fb4c47544fac0e647117b1c4b65
Fabien Dubin <fabien.dubin@gmail.com>
2024-12-13 18:53:07 +0100

    Collection page in progress

--- src/pages/CollectionPage/MyCollection.jsx
+++ src/pages/CollectionPage/MyCollection.jsx
@@ -37,7 +37,7 @@ const MyCollection = () => {
   //Redirect the user to the image page
   const handleImageClick = (index) => {
     const imgId = userCollection[index].id;
-    nav(`/image/${imgId}`);
+    nav(`/for-frodo/${imgId}`);
   };
 
   useEffect(() => {
