d2bb75474bf3dd5c8f57b77ac8d165beb8f9f906
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2024-12-17 11:45:17 +0100

    Added 404 Page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import memeImage from '../../assets/404 Meme.jpg'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
+    <>
+      <Card className='w-[85vw] max-w-[500px] h-[350px] bg-white rounded-2xl shadow-lg overflow-hidden m-auto mt-5 '>
+        <img
+          className='w-full h-full '
+          src={memeImage}
+          alt='The Image goes here'
+        />
+      </Card>
+      <div className='flex flex-col items-center mt-5'>
+        <p className='text-2xl font-bold text-inherit mb-2'>
+          The Eagles are coming to return to your little hobbit hole{' '}
+        </p>
+        <Button className='text-2xl font-bold mb-6 p-5  bg-slate-400 hover:text-emerald-700'>
+          <Link to='/'>Fly you Fools</Link>
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default NotFound
