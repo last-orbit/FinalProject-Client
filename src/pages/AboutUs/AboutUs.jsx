6e2761e157e7220ec071bf63997cf768af46d36f
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2024-12-19 11:18:24 +0100

    Finished About Us Page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -3,21 +3,94 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
-  CardHeader,
+
   CardTitle,
 } from '@/components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import fabImage from '../../assets/Fab-Image.png'
+import robImage from '../../assets/2ndProfile.jpg'
+import { Link, NavLink } from 'react-router-dom';
+// Icons are deprecated so they may have to be change later or write a ternanry instead
+import { Linkedin, Github } from 'lucide-react';
+
+
+
 
 const AboutUs = () => {
   return (
     <div className='min-h-screen'>
-      <h2 className='text-2xl mt-5'>Welcome to the fellowship</h2>
+      <h2 className='text-2xl mt-5 capitalize'>Welcome to the fellowship</h2>
       <h3 className='text-xl mt-2'>About Us</h3>
-      <div className='grid grid-cols-2 mt-4 w-11/12 mx-auto'>
-         <Card>
-          <CardHeader>card header</CardHeader>
+      <div className='grid grid-cols-2 mt-4 w-11/12 mx-auto gap-10'>
+        <Card>
+          <CardTitle className='mt-2'>Fab</CardTitle>
+          <CardContent>
+            <Avatar className='w-32 h-32 mt-4 mx-auto'>
+              <AvatarImage
+                className='rounded-full border-2  border-gray-100 object-cover'
+                src={fabImage}
+              />
+              <AvatarFallback>??</AvatarFallback>
+            </Avatar>
+            <CardDescription className='mt-2'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              non.
+            </CardDescription>
+            <h4>Here are my links below : </h4>
+            <div className='flex gap-4 justify-center mt-3'>
+              <Link
+                className='underline'
+                to='https://www.linkedin.com/in/fabien-dubin-46ab121b/'
+              >
+                {' '}
+                <Linkedin />
+              </Link>
+              <Link className='underline' to='https://github.com/FabienDubin'>
+                {' '}
+                <Github />
+              </Link>
+            </div>
+          </CardContent>
         </Card>
-        <Card>text 2</Card>
+        <Card>
+          <CardTitle className='mt-2'>Rob</CardTitle>
+          <CardContent>
+            <Avatar className='w-32 h-32 mt-4 mx-auto'>
+              <AvatarImage
+                className='rounded-full border-2  border-gray-100 object-cover'
+                src={robImage}
+              />
+              <AvatarFallback>??</AvatarFallback>
+            </Avatar>
+            <CardDescription className='mt-2'>
+              Former Theatre Technical Director turned Web Developer
+            </CardDescription>
+            <h4>Here are my links below : </h4>
+
+            <div className='flex gap-4 justify-center mt-3'>
+              <Link
+                className='underline'
+                to='https://www.linkedin.com/in/dev-ro/'
+              >
+                {' '}
+                <Linkedin />
+              </Link>
+              <Link className='underline' to='https://github.com/last-orbit'>
+                {' '}
+                <Github />
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <h3 className='text-xl mt-4'>Github Repos</h3>
+      <div className='flex gap-4 w-11/12 justify-center mx-auto mt-4'>
+        <p>
+          The Front End Repo <Link className='underline'>Here</Link>
+        </p>
+        <p>
+          The Back End Repo <Link className='underline'>Here</Link>
+        </p>
       </div>
     </div>
   );
